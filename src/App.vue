48564032be849814599b77287701bfc0c7a02b02
Bernhard Landsberg <bernhardlandsberg@Bernhards-MacBook-Pro.local>
2018-03-06 17:54:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.svg">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted () {
+    this.$lx.init()
+      .then(setup => {
+        this.$lx.ready({})
+      })
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+  #app
+    display flex
+    flex-flow column
+    justify-content center
+    align-items center
+</style>
