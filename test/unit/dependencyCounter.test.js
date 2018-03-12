95040e45019d181af64d39db38bd230b004549a4
Bernhard Landsberg <bernhardlandsberg@Bernhards-MacBook-Pro.local>
2018-03-12 17:16:25 +0100

    Added sample landscapes

--- test/unit/dependencyCounter.test.js
+++ test/unit/dependencyCounter.test.js
@@ -1,9 +1,41 @@
 var samples = require('../../data/sampleStructures')
-var count = require('../../src/dependencyCounter')
+var providingMatrix = require('../../src/dependencyCounter')
 
-// Rename count
-test('adds 1 + 2 to equal 3', () => {
-    expect(count(samples.interfaceStructureZ1)).toEqual({
-        2: [1, 3]
-    })
+test('Sample data Z1 expected result', () => {
+  expect(providingMatrix(samples.interfaceStructureZ1)).toEqual({
+    2: [1, 3]
   })
+})
+
+test('Sample data Z2 expected result', () => {
+  expect(providingMatrix(samples.interfaceStructureZ2)).toEqual({
+    1: [3],
+    2: [1],
+    3: [2]
+  })
+})
+
+test('Sample data Z3 expected result', () => {
+  expect(providingMatrix(samples.interfaceStructureZ3)).toEqual({
+    1: [3],
+    2: [1]
+  })
+})
+
+test('Sample data Z4 expected result', () => {
+  expect(providingMatrix(samples.interfaceStructureZ4)).toEqual({
+    2: [1],
+    3: [2],
+    4: [2]
+  })
+})
+
+test('Sample data Z8 expected result', () => {
+  expect(providingMatrix(samples.interfaceStructureZ8)).toEqual({
+    1: [2],
+    2: [3, 4],
+    3: [1],
+    4: [5],
+    5: [2]
+  })
+})
